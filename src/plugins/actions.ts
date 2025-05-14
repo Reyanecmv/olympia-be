e5e118db65cb37b0e28232b4753e4929297e48dc
Vlad Cristea <devcmv@gmail.com>
2025-05-14 23:16:06 +0300

    initial commit

--- src/plugins/actions.ts
+++ src/plugins/actions.ts
@@ -5,7 +5,7 @@ import { readdir } from "fs/promises";
 import { join, parse } from "path";
 import { Action, globalActionTypes } from "@app-types/app.js";
 
-const actionsPath = join(process.cwd(), "disc/src/actions");
+const actionsPath = join(process.cwd(), "dist/src/actions");
 const actions = new Map<string, Resolver<Action>>();
 
 function createActionResolver<T extends Action>(
